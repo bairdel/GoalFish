0eee50b1631557fcb67785038ef7c5d3b2e75a55
bairdel <97924634+bairdel@users.noreply.github.com>
2022-08-31 01:41:42 +0100

    Progress made on widget

--- app/src/main/java/com/example/goalfish/WidgetProvider.java
+++ app/src/main/java/com/example/goalfish/WidgetProvider.java
@@ -16,20 +16,25 @@ public class WidgetProvider extends AppWidgetProvider {
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
         for (int appWidgetId : appWidgetIds) {
 
-//            DB = new DBHelper(this);
-//            TextView t = (TextView) findViewById(R.id.widgetWord);
-//            String c = (String.valueOf(DB.getCum()));
-//            t.setText(c);
-//
-//            TextView t2 = (TextView) findViewById(R.id.widgetGoal);
-//            String c2 = (String.valueOf(DB.getGoal()));
-//            t2.setText(c2);
+            DB = new DBHelper(context);
+            //TextView t = (TextView) findViewById(R.id.widgetWord);
+            String c = (String.valueOf(DB.getCum("Default Goal")));
+            //t.setText(c);
+
+            //TextView t2 = (TextView) findViewById(R.id.widgetGoal);
+            String c2 = (String.valueOf(DB.getGoal("Default Goal").get("Goal")));
+            //t2.setText(c2);
+
+
 
             Intent intent = new Intent(context, MainActivity.class);
             PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
 
             RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.progress_widget);
             views.setOnClickPendingIntent(R.id.widgetButton, pendingIntent);
+            views.setProgressBar(R.id.progressBar, Integer.parseInt(c2), Integer.parseInt(c), false);
+            views.setTextViewText(R.id.widgetWord, c);
+            views.setTextViewText(R.id.widgetGoal, c2);
 
             appWidgetManager.updateAppWidget(appWidgetId, views);
         }
